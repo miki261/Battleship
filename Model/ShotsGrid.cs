203b24fe59246fc1c152a98f47e80c8215f5ca5a
Paolo Stabile <pastabil@vsite.hr>
2025-05-28 21:38:13 +0200

    Fixed errors.

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -1,5 +1,13 @@
 ﻿namespace Model;
 
+public enum Direction
+{
+    Upwards,
+    Rightwards,
+    Downwards,
+    Leftwards
+}
+
 public class ShotsGrid : Grid
 {
     public ShotsGrid(int rows, int columns) : base(rows, columns)
@@ -14,5 +22,42 @@ public class ShotsGrid : Grid
     public Square GetSquare(int row, int column)
     {
         return squares[row, column]!;
+    }
+
+    public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction direction)
+    {
+        var result = new List<Square>();
+
+        int deltaRow = 0;
+        int deltaColumn = 0;
+        int limit = 0;
+        switch (direction)
+        {
+            case Direction.Upwards:
+                --row;
+                deltaRow = -1;
+                limit = -1;
+                break;
+            case Direction.Rightwards:
+                ++column;
+                deltaColumn = +1;
+                limit = Columns;
+                break;
+            case Direction.Downwards:
+                ++row;
+                deltaRow = +1;
+                limit = Rows;
+                break;
+            case Direction.Leftwards:
+                --column;
+                deltaColumn = -1;
+                limit = -1;
+                break;
+        }
+        for (int r = row, c = column; r != limit && c != limit && IsSquareAvailable(r, c); r += deltaRow, c += deltaColumn)
+        {
+            result.Add(squares[r, c]!);
+        }
+        return result;
     }
 }
